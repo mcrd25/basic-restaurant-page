01d3230d4fde56f5ce6171fb178c426bc3baacb1
mcrd25 <mayacrd25@gmail.com>
2019-09-02 05:24:36 -0400

    Added new Main page instance and method to switch content of main

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import M from 'materialize-css';
 import MainModule from './components/mainModule';
+import MainPage from './components/MainPage';
 
 const slider = document.querySelector('.slider');
 const mainDiv = document.querySelector('#content');
 const sideNav = document.querySelector('.sidenav');
 
 const mainModule = new MainModule(mainDiv);
+const mainPage = new MainPage();
 
 const generateMainSkeleton = () => {
   mainModule.createNav();
   mainModule.createMain();
   mainModule.createFooter();
 };
-// const currentContent = (content) => {
-//   const main = document.querySelector('main');
-//   main.innerHTML = content;
-// };
+const currentContent = (content) => {
+  const main = document.querySelector('main');
+  console.log(main);
+  main.innerHTML = '<div>TEsting123</div>';
+};
 // const changeContent = () => {
 // }
 const render = () => {
   generateMainSkeleton();
+  currentContent('<div>Testing 123456436546u35689  u36 45u6 jhdfghjbdsfgjhbsdfjgb jdbghjdbvgjhbdvjgbdfjgvbsjdhd  </div>');
 };
 
 const initSlider = (slider) => {
