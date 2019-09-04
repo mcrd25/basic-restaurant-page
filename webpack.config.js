a29d22352bc67a90a5e096b9776430c695d998b9
mcrd25 <mayacrd25@gmail.com>
2019-09-03 20:10:11 -0400

    fixed two linter errors from webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [{
@@ -13,7 +13,7 @@ module.exports = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
+          presets: ['@babel/preset-env'],
         },
       },
     }],
